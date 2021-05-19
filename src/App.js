3a2538c06ba76c79bb78d24d343de5f626dbaecc
Ernesferre <70835371+Ernesferre@users.noreply.github.com>
2021-05-19 13:23:59 -0300

    insert loguin form using formik library

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import FormScreen from "./components/FormScreen";
 import HomeScreen from "./components/HomeScreen";
 import LoginScreen from "./components/LoginScreen";
 import AppRouter from "./routers/AppRouter";
+import './App.css';
 
 
 function App() {
   return (
-    <>
+    <div className="container">
       <AppRouter/>
-    </>
-
-  
+    </div>
   );
 }
 
--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,25 +1,63 @@
 import React from 'react'
+import { Formik, Form } from 'formik';
+import TextField from './TextField';
+import * as Yup from 'yup';
+
 
 const LoginScreen = ({ history }) => {
 
-    const handleLogin = () => {
-        history.replace('/')
-    }
+    const validate = Yup.object({
+        email: Yup.string()
+            .email("Email is Invalid")
+            .required("Email is required"),
+        password: Yup.string()
+            .required("Password is required"),
+    })
+
+    // const handleLogin = () => {
+    //     history.replace('/')
+    // }
 
 
     return (
-        
-            <div className="container">
-                <h1>Login</h1>
-                <hr/>
+        <div className="container mt-5">
+            <div className="col-md-4 mx-auto">
+            <Formik
+                initialValues={{
+                    email: '',
+                    password: '',
+                }}
+            validationSchema={validate}
+            onSubmit={values => {
+                console.log(values)
+            }}
 
-                <button
-                    className="btn btn-primary"
-                    onClick={handleLogin}
-                >
-                    Login
-                </button>
+            >
+                {formik => (
+                    <div>
+                        <h1 className="my-4.font-weight-bold-display-4 text-center"> Login </h1>
+                        
+                        <Form>
+
+                            <TextField label="Email" name="email" type="email" />
+                            <TextField label="Password" name="password" type="password" />
+                            <button className="btn btn-success mt-3 ml-3 " type="submit">Enter</button>
+                            <button className="btn btn-danger mt-3 ml-5 " type="reset">Reset</button>
+
+                        
+
+                        </Form>
+                    </div>
+                )}
+            </Formik>
             </div>
+        </div>
+
+                
+
+                   
+                
+            
         
     )
 }
